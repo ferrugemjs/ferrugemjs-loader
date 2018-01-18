295eef9fb7e89bd54c5b39977a65c409002c65d6
alexandre.araujo <alexandre.araujo@mv.com.br>
2018-01-18 09:24:54 -0300

    fixed template default value

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var fjsparse = require("ferrugemjs-node");
 module.exports = function(source) {
     var pathTmp = this.resourcePath.match(/(\w|[-.])+$/g)[0];
     pathTmp = pathTmp.substring(0,pathTmp.lastIndexOf("."));
-    return fjsparse(source,{viewModel:pathTmp,templateExtension:this.query.templateExtension})
+    return fjsparse(source,{viewModel:pathTmp,templateExtension:this.query.templateExtension || ".html"})
 };
