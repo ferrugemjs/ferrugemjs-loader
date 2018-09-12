95711ca4796d43009317a6119f59aaa7d3a441aa
ferrugemjs <ferrugemjavascript@gmail.com>
2018-09-11 23:11:53 -0300

    feat: add component path in development mode

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var fjsparse = require("ferrugemjs-node");
+var path = require('path');
 
 module.exports = function(source) {
     var pathTmp = this.resourcePath.match(/(\w|[-.])+$/g)[0];
     pathTmp = pathTmp.substring(0,pathTmp.lastIndexOf("."));
-    return fjsparse(source,{viewModel:pathTmp,templateExtension:this.query.templateExtension || ".html"})
+    return fjsparse(source,{
+        env:this.query.env || "development",
+        resourcePath: path.relative(__dirname, this.resourcePath).replace('../../',''),
+        viewModel:pathTmp,
+        templateExtension:this.query.templateExtension || ".html"
+    })
 };
