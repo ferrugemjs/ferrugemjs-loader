db0514f10d7656a711f9cb9161d3376ef74d2c74
ferrugemjs <ferrugemjavascript@gmail.com>
2023-02-20 17:28:06 -0300

    fix: compile version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var fjsparse = require("ferrugemjs-node");
 var path = require('path');
+var fjsparse = require("@ferrugemjs/compile/parse/parse");
 
 module.exports = function (source) {
     var pathTmp = this.resourcePath.match(/(\w|[-.])+$/g)[0];
     pathTmp = pathTmp.substring(0, pathTmp.lastIndexOf("."));
-    return fjsparse(source, {
+    return fjsparse.default(source, {
         env: this.query.env || "development",
         resourcePath: path.relative(__dirname, this.resourcePath).replace('../../', ''),
         viewModel: pathTmp,
